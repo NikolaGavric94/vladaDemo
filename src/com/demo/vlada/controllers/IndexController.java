6a51f72626cad900978c35ac8703ccf0899fe0a1
Nikola Gavric <nikola.gavric94@gmail.com>
2016-06-20 11:53:29 +0200

    Configured whole project, for now..

--- src/com/demo/vlada/controllers/IndexController.java
+++ src/com/demo/vlada/controllers/IndexController.java
@@ -1,5 +1,16 @@
 package com.demo.vlada.controllers;
 
-public class IndexController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value="/")
+public class IndexController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String indexPage() {
+		return "index";
+	}
+	
 }
