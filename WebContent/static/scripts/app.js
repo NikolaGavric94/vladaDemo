84b6e34864c1d37423ada0808b4ec2c03a16a32b
Nikola Gavric <nikola.gavric94@gmail.com>
2016-06-27 13:51:45 +0200

    New structure

---
+++ WebContent/static/scripts/app.js
@@ -0,0 +1,34 @@
+angular
+   .module('DynFiles', [
+      'ngRoute',
+      'routeStyles',
+      'ngTable',
+      'ui.ace',
+      'angularFileUpload'
+   ])
+   .config(function($routeProvider, $locationProvider, $httpProvider) {
+      $routeProvider
+         .when('/', {
+            templateUrl: 'static/views/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'vm'
+         })
+         .when('/home', {
+            templateUrl: 'static/views/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'vm'
+         })
+         .when('/calculate', {
+            templateUrl: 'static/views/calculate.html',
+            controller: 'CalculateCtrl',
+            controllerAs: 'vm'
+         })
+         .when('/uploadFile', {
+            templateUrl: 'static/views/upload_file.html',
+            controller: 'UploadFileCtrl',
+            controllerAs: 'vm'
+         });
+   })
+   .run(function($rootScope) {
+      $rootScope.ajaxHostPrefix = 'http://localhost:8080/vladaDemo/rest/';
+   });
